b4b6cf613f067b116ec364dd13ad85744d63353d
Arimi-San <gomitaxdxd@gmail.com>
2023-06-24 10:34:33 -0600

    si

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Holaaaaaaaa <code>src/App.js</code> and save to relqwoad.
+          Holaaaaaaaa :D <code>src/App.js</code> and save to relqwoad.
         </p>
         <a
           className="App-link"
